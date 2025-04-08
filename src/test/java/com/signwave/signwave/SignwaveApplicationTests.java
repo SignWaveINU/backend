a0a7f3127c1152f1e0983e43be3467cf7420022c
Jimin15 <sujean07015@gmail.com>
2025-04-09 01:44:54 +0900

    Initial commit: SignWave Spring Boot backend with Docker

---
+++ src/test/java/com/signwave/signwave/SignwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signwave.signwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
