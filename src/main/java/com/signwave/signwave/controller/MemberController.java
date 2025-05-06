1707392105935f80d7575f955a6fe1d8d795f721
Jimin15 <sujean07015@gmail.com>
2025-05-07 04:20:10 +0900

    feat: 회원가입 컨트롤러 구현

---
+++ src/main/java/com/signwave/signwave/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.signwave.signwave.controller;
+
+import com.signwave.signwave.dto.SignUpRequest;
+import com.signwave.signwave.dto.SignUpResponse;
+import com.signwave.signwave.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Auth", description = "회원가입 및 인증 관련 API")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "이메일, 닉네임, 비밀번호로 새로운 사용자를 등록합니다.")
+    public ResponseEntity<SignUpResponse> signup(@RequestBody SignUpRequest request) {
+        SignUpResponse response = memberService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
