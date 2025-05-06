c7de871a050c75ec53490d99ee51961e7d25c885
Jimin15 <sujean07015@gmail.com>
2025-05-07 05:31:49 +0900

    feat: Spring Security 설정 및 JWT 인증 필터 등록

--- src/main/java/com/signwave/signwave/config/SecurityConfig.java
+++ src/main/java/com/signwave/signwave/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.signwave.signwave.config;
 
+import com.signwave.signwave.jwt.JwtAuthenticationFilter;
+import com.signwave.signwave.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,12 +10,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
@@ -20,10 +28,10 @@ public class SecurityConfig {
                                 "/translate",
                                 "/auth/**"
                         ).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
