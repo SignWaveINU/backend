5cb7acb2c2170ded0d4dfabb58b2edfaf5cbdb3f
Jimin15 <sujean07015@gmail.com>
2025-05-07 04:09:27 +0900

    feat: BCryptPasswordEncoder 빈 등록으로 비밀번호 암호화 설정

--- src/main/java/com/signwave/signwave/config/SecurityConfig.java
+++ src/main/java/com/signwave/signwave/config/SecurityConfig.java
@@ -4,8 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
@@ -24,5 +25,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    //비밀번호 암호화용 빈 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
