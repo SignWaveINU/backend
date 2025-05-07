a1d9c964be38c208192f9d7ed925d53ddca4df7c
Jimin15 <sujean07015@gmail.com>
2025-05-08 06:28:34 +0900

    feat: findByMember 메서드 추가 in TranslationHistoryRepository

--- src/main/java/com/signwave/signwave/dto/TranslationHistoryResponse.java
+++ src/main/java/com/signwave/signwave/dto/TranslationHistoryResponse.java
@@ -1,2 +1,22 @@
-package com.signwave.signwave.dto;public class TranslationHistoryResponse {
+package com.signwave.signwave.dto;
+
+import com.signwave.signwave.entity.TranslationHistory;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TranslationHistoryResponse {
+
+    private Long id;
+    private String translatedText;
+    private boolean isFavorite;
+
+    public static TranslationHistoryResponse fromEntity(TranslationHistory history) {
+        return TranslationHistoryResponse.builder()
+                .id(history.getId())
+                .translatedText(history.getSignLanguageTranslation().getTranslatedText())
+                .isFavorite(history.isFavorite())
+                .build();
+    }
 }
