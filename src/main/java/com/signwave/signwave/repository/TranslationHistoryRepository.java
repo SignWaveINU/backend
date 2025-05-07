8f24119ae4bd428ac616175e6442905f6f51b786
Jimin15 <sujean07015@gmail.com>
2025-05-08 06:28:34 +0900

    feat: findByMember 메서드 추가 in TranslationHistoryRepository

--- src/main/java/com/signwave/signwave/repository/TranslationHistoryRepository.java
+++ src/main/java/com/signwave/signwave/repository/TranslationHistoryRepository.java
@@ -1,9 +1,11 @@
 package com.signwave.signwave.repository;
 
+import com.signwave.signwave.entity.Member;
 import com.signwave.signwave.entity.TranslationHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface TranslationHistoryRepository extends JpaRepository<TranslationHistory, Long> {
+    List<TranslationHistory> findByMember(Member member);
 }
