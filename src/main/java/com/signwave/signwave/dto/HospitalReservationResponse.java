f9c7610170dc8a09b357af309b2c7fb1552d095a
Jimin15 <sujean07015@gmail.com>
2025-05-08 17:53:31 +0900

    feat: 병원 예약 기능을 위한 DTO 클래스 추가

--- src/main/java/com/signwave/signwave/dto/HospitalReservationResponse.java
+++ src/main/java/com/signwave/signwave/dto/HospitalReservationResponse.java
@@ -1,2 +1,12 @@
-package com.signwave.signwave.dto;public class HospitalReservationResponse {
+package com.signwave.signwave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HospitalReservationResponse {
+    private Long reservationId;
+    private String hospitalName;
+    private String reservationTime;
 }
