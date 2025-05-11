9a782b95df1d1e521938ea92307a10f1c2ce24b0
Jimin15 <sujean07015@gmail.com>
2025-05-12 04:44:54 +0900

    feat: TranslationHistoryRepository에 즐겨찾기된 기록을 최신순 조회하는 findByMemberAndIsFavoriteTrueOrderByCreatedAtDesc 메서드 추가

--- src/main/java/com/signwave/signwave/repository/TranslationHistoryRepository.java
+++ src/main/java/com/signwave/signwave/repository/TranslationHistoryRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface TranslationHistoryRepository extends JpaRepository<TranslationHistory, Long> {
     List<TranslationHistory> findByMember(Member member);
+    List<TranslationHistory> findByMemberAndIsFavoriteTrueOrderByCreatedAtDesc(Member member);
 }
