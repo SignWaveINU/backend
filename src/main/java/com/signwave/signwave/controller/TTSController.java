7fbf0696efa1035374bc10870db0d68bef703298
Jimin15 <sujean07015@gmail.com>
2025-05-15 07:58:12 +0900

    feat : [fix] TTSController에서 인증된 사용자 정보(Member) 전달하도록 수정

--- src/main/java/com/signwave/signwave/controller/TTSController.java
+++ src/main/java/com/signwave/signwave/controller/TTSController.java
@@ -2,10 +2,13 @@ package com.signwave.signwave.controller;
 
 import com.signwave.signwave.dto.SentenceRequest;
 import com.signwave.signwave.dto.GestureTranslationResponse;
+import com.signwave.signwave.entity.Member;
 import com.signwave.signwave.service.TTSService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +21,9 @@ public class TTSController {
     @PostMapping
     @Operation(summary = "문장 → TTS 변환", description = "문장을 입력받아 TTS(mp3 base64)를 생성하고 S3 URL을 반환합니다.")
     public ResponseEntity<GestureTranslationResponse> generateTTS(@RequestBody SentenceRequest request) {
-        return ResponseEntity.ok(service.generate(request.getSentence()));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Member member = (Member) auth.getPrincipal();
+
+        return ResponseEntity.ok(service.generate(request.getSentence(), member));
     }
 }
